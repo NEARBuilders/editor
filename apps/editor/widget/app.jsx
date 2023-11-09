ae26079b39e64d623a72f4b0bc9ed8a13155baa0
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2023-11-09 06:02:33 +0000

    refactor: view, edit

--- apps/editor/widget/app.jsx
+++ apps/editor/widget/app.jsx
@@ -37,14 +37,23 @@ function PanelHeader({ options, onChange }) {
         {options &&
           options.map((it) => <Option value={it.value}>{it.label}</Option>)}
       </Select>
-      <Button>Click Me</Button>
+      <Button onClick={() => Social.set({
+        index: {
+          every: JSON.stringify({
+            key: "thing", // type
+            value: {
+              src: "efiz.near/widget/Tree"
+            }
+          })
+        }
+      })}>Publish</Button>
     </Header>
   );
 }
 
 const [editorValue, setEditorValue] = useState("");
-const [editorSrc, setEditorSrc] = useState("editor.near/widget/edit.Markdown");
-const [viewerSrc, setViewerSrc] = useState("editor.near/widget/view.Markdown");
+const [editorSrc, setEditorSrc] = useState("editor.near/widget/markdown.edit");
+const [viewerSrc, setViewerSrc] = useState("editor.near/widget/markdown.view");
 
 function handleEditorChange(value) {
   setEditorValue(value);
@@ -73,9 +82,9 @@ return (
     <Panel>
       <PanelHeader
         options={[
-          { value: "editor.near/widget/edit.Markdown", label: "Markdown" },
-          { value: "editor.near/widget/edit.Code", label: "Code" },
-          { value: "editor.near/widget/edit.Canvas", label: "Canvas" },
+          { value: "editor.near/widget/markdown.edit", label: "Markdown" },
+          { value: "editor.near/widget/code.edit", label: "Code" },
+          { value: "editor.near/widget/canvas.edit", label: "Canvas" },
         ]}
         onChange={handleEditorSrcChange}
       />
@@ -86,9 +95,9 @@ return (
     <Panel>
       <PanelHeader
         options={[
-          { value: "editor.near/widget/view.Markdown", label: "Markdown" },
-          { value: "editor.near/widget/view.Code", label: "Code" },
-          { value: "editor.near/widget/view.Canvas", label: "Canvas" },
+          { value: "editor.near/widget/markdown.view", label: "Markdown" },
+          { value: "editor.near/widget/code.view", label: "Code" },
+          { value: "editor.near/widget/canvas.view", label: "Canvas" },
         ]}
         onChange={handleViewerSrcChange}
       />
